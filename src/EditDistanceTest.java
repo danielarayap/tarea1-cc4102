20d2c599c6b5b2c7c2177793bea372aa80fa377c
danielarayap1 <daniel.arayap1@gmail.com>
2019-09-28 03:50:06 -0300

    midiendo tiempo

--- src/EditDistanceTest.java
+++ src/EditDistanceTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.Before;
 
 public class EditDistanceTest{
+    private int[] M;
     private EditDistanceAlgorithm algorithm;
     private String string1, string2;
 
@@ -14,6 +15,7 @@ public class EditDistanceTest{
         algorithm = new EditDistanceAlgorithm();
         int min = (int) Math.pow(2, 10);
         int max = (int) Math.pow(2, 13);
+        M = new int[]{20 * min, 40 * min, 80 * min};
         int len1 = ThreadLocalRandom.current().nextInt(min, max + 1);
         int len2 = ThreadLocalRandom.current().nextInt(min, max + 1);
         string1 = sg.generateString(len1);
@@ -24,7 +26,10 @@ public class EditDistanceTest{
     public void testAlgorithm() {
         System.out.println(string1);
         System.out.println(string2);
+        long startTime = System.currentTimeMillis();
         int result = algorithm.computeEditDistance(string1, string2);
+        long endTime = System.currentTimeMillis();
+        System.out.println(endTime-startTime);
         System.out.println(result);
     }
 
