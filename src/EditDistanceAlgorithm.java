cc61d4c288a634f22ac43424aedbb5490ecf21f9
scisneros <sebastian.cisneros384@gmail.com>
2019-09-28 21:10:01 -0300

    Added space efficient RAM algorithm

--- src/EditDistanceAlgorithm.java
+++ src/EditDistanceAlgorithm.java
@@ -19,6 +19,28 @@ class EditDistanceAlgorithm {
         return graphMatrix[n-1][m-1];
     }
 
+    int computeEditDistanceSpaceEfficient(String s1, String s2) {
+        int n = s1.length() + 1;
+        int m = s2.length() + 1;
+        int[] prevRow = new int[n];
+        int[] thisRow = new int[n];
+        for (int i = 0; i < n; i++) {
+            prevRow[i] = i;
+        }
+        for (int j = 1; j < m; j++) {
+            thisRow = new int[n];
+            thisRow[0] = j;
+            for (int i = 1; i < n; i++) {
+                if (s1.charAt(i-1) == s2.charAt(j-1))
+                    thisRow[i] = prevRow[i-1];
+                else
+                    thisRow[i] = Math.min(thisRow[i-1], Math.min(prevRow[i-1], prevRow[i])) + 1;
+            }
+            prevRow = thisRow;
+        }
+        return thisRow[n-1];
+    }
+
     /*
     public static void main(String[] args) {
         int result = computeEditDistance("can", "pan");
